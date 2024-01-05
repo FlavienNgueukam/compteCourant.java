08d1c26766e78ba0d63002d0a3e304f89bbad492
FlavienNgueukam <briatorngueukam@gmail.com>
2024-01-05 10:44:09 -0800

    initial file

---
+++ CompteCourant.java
@@ -0,0 +1,34 @@
+public class CompteCourant {
+
+    // attributs
+    private String nomProprietaire;
+    private int montant;
+    private int decouvertAutorise;
+
+    // constructeur
+    public CompteCourant(String nomProprietaire, int montant){
+        this.nomProprietaire = nomProprietaire;
+        this.montant = montant;
+        this.decouvertAutorise = 0;
+    }
+
+    // methodes
+    public void afficherSolde(){
+        System.out.println("Solde : " + this.montant + "€");
+    }
+
+    public void deposerArgent(int montant){
+        this.montant += montant;
+    }
+
+    public void retirerArgent(int montant){
+        int montantApresCalcul = this.montant - montant;
+        if(montantApresCalcul >= this.decouvertAutorise){
+            this.montant -= montant;
+        }else{
+            System.out.println("Vous n'avez pas les fonds !");
+        }
+    }
+
+
+}
